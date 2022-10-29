0ba993e1e302ce0a8a301ae26b03cd16fc218d47
adelekesamuel <59022263+adelekesamuel@users.noreply.github.com>
2022-10-29 18:05:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form class="col-md-4">
+          <div class="mb-2">
+            <label for="inputFirstName" class="form-label">Name</label>
+            <input type="text" class="form-control" id="inputFirstName"></input>
+          </div>
+
+          <div class="mb-2">
+            <label for="inputLastName" class="form-label">Last Name</label>
+            <input type="text" class="form-control" id="inputLastName"></input>
+          </div>
+
+          <div class="mb-2">
+            <label for="inputAge" class="form-label">Age</label>
+            <input type="number" class="form-control" id="inputAge"></input>
+          </div>
+
+          <div class="mb-2">
+            <label for="inputContact" class="form-label">Contact</label>
+            <input type="number" class="form-control" id="inputContact"></input>
+          </div>
+
+          <div class="mb-2">
+            <label for="inputAddress" class="form-label">Address</label>
+            <input type="text" class="form-control" id="inputAddress"></input>
+          </div>
+
+          <div class="mb-2">
+            <label for="inputEmail" class="form-label">Email Address</label>
+            <input type="email" class="form-control" id="inputEmail"></input>
+            <div id="emailText" class="form-text">Yor email is privately stored on our server</div>
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </header>
     </div>
   );
